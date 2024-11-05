805db9a6d8706e29ab7a2fc54549f453baa7ef5d
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-11-05 23:06:57 +0100

    yeh

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from 'express'
 import TemplateRoute from './routes/Template.js';
 import cors from 'cors'
 const app = express();
-app.use(cors({origin:'http://localhost:3000',methods:["GET","POST","DELETE","PUT","OPTIONS"],allowedHeaders:['Content-Type', 'Authorization']}));
+app.use(cors({origin:['https://portgen-frontend.vercel.app/','http://localhost:3000'],methods:["GET","POST","DELETE","PUT","OPTIONS"],allowedHeaders:['Content-Type', 'Authorization']}));
+
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/Template',TemplateRoute);
