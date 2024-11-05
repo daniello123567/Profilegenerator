4b4b5a9fdffa53df35dd00e1513d863ca2911d6b
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-11-05 22:39:59 +0100

    yeah

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import TemplateRoute from './routes/Template.js';
 import cors from 'cors'
 const app = express();
-app.use(cors({origin:'*'}));
+app.use(cors({origin:'http://localhost:3000'}));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use('/Template',TemplateRoute);
