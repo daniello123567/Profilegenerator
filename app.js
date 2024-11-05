7a32b92b6e978bfcc1093f390b3593037aca2f6a
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2024-11-05 22:28:15 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import TemplateRoute from './routes/Template.js';
+import cors from 'cors'
+const app = express();
+app.use(cors({origin:'*'}));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use('/Template',TemplateRoute);
+app.listen(3001, ()=>{
+  console.log('port don start');
+})
---
+++ routes/Template.js
@@ -0,0 +1,70 @@
+import express from 'express'
+import fetch from 'node-fetch';
+import ReturnNextjsFiles from '../utils/Files.js';
+import { NameCanBeAlias } from '../utils/utils.js';
+const TemplateRoute = express.Router()
+TemplateRoute.route('/')
+   .post(async (req, res) => {
+    const { ImageUrl,NamesofUser,workofuser,Heromsg,Herosummary,Email,Skills,Projects } = req.body;
+
+    if(!ImageUrl||!NamesofUser||!workofuser||!Heromsg||!Email||!Skills||!Projects)res.status(400).send('incomplete info')
+    const processedName = String(NamesofUser).split(' ').join('-');
+
+    const files = ReturnNextjsFiles(ImageUrl,NamesofUser,workofuser,Heromsg,Herosummary,Email,Skills,Projects);
+    const aliase = await NameCanBeAlias(processedName);
+
+    const Deploy = async ()=>{
+    const response = await fetch("https://api.vercel.com/v13/deployments",{
+    method:"POST",
+    headers:{
+      "Authorization":`Bearer cDdJr8A0iejkRn9UECLTvwJS`,
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      alias:[`${aliase}`],
+      public:true,
+      name:`${processedName.toLowerCase()}`,
+      files:files,
+      projectSettings:{
+        framework:"nextjs"
+      }
+    })
+   })
+   const data = await response.json();
+
+   return data.project.id
+}
+let projectid = await Deploy();
+const CheckRedyState = async()=>{
+  const response = await fetch(`https://api.vercel.com/v13/projects/${projectid}`,{
+   method:'GET',
+   headers:{
+     "Authorization":'Bearer cDdJr8A0iejkRn9UECLTvwJS',
+     "Content-Type":'application/json'
+   }
+  });
+  const project = await response.json();
+  return project.targets.production.readyState.toString()
+}
+const ResolveWhenReadyState = async ()=>{
+  return new Promise(async (resolve,reject)=>{
+    const checkingForBuild = setInterval(async ()=>{
+      let readyState = CheckRedyState();
+      if(await readyState==='READY'){
+        clearInterval(checkingForBuild);
+        res.json({yournewurl:aliase,status:'ok'})
+      }else{
+        console.log(await readyState);
+      }
+    },2000);
+
+
+  })
+}
+
+ResolveWhenReadyState()
+
+
+  });
+
+export default TemplateRoute;
